8fe559f674670e3f5767e7ef0688626bf5f088d6
Prajwal Agrawal <prajwalagrawal1348@gmail.com>
2026-06-28 00:11:23 +0545

    Remove basePath to support custom root domain deployment

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-portfolio",
   transpilePackages: ["three"],
   images: {
     unoptimized: true,
